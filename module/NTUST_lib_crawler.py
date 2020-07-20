528113c3bc2c65d2fefc10653d0a4ca9f21e99c7
Dada <40861818+dada00321@users.noreply.github.com>
2020-07-21 02:33:42 +0800

    Update NTUST_lib_crawler.py

--- module/NTUST_lib_crawler.py
+++ module/NTUST_lib_crawler.py
@@ -10,7 +10,6 @@ class Webdriver():
         chrome_options = Options()
         if headless == True:
             chrome_options.add_argument("--headless")
-            #chrome_options.headless = True # also works
         wd_path = r"D:\geckodriver\chromedriver.exe"
         driver = wd.Chrome(wd_path, options=chrome_options)
         driver.implicitly_wait(10)
