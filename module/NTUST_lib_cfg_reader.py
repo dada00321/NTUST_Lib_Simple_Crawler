8d5f61b268a4bb6ca0dd8d2756d256287091c3ba
Dada <40861818+dada00321@users.noreply.github.com>
2020-07-21 02:32:25 +0800

    Update NTUST_lib_cfg_reader.py

--- module/NTUST_lib_cfg_reader.py
+++ module/NTUST_lib_cfg_reader.py
@@ -2,13 +2,7 @@ import configparser
 
 def read_cfg():
     conf = configparser.ConfigParser()
-    #cfg_path = r"../res/cfg.ini"
     cfg_path = r"res/cfg.ini"
     conf.read(cfg_path, encoding="utf-8")
     items = conf.items(conf.sections()[0])
-    #print(items[1][1][1:-1])
     return [items[0][1][1:-1], items[1][1][1:-1]]
-'''
-if __name__ == "__main__":
-    read_cfg()
-'''
